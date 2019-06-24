e3767e749636fa35be8916d5d3301395b523a5a4
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-24 14:47:14 -0400

    challenge done

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -1,8 +1,33 @@
 package com.dsimon;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int numberCounter = 1;
+        int sum = 0;
+
+        while (numberCounter <= 10) {
+            System.out.println("Enter number #" + numberCounter);
+
+            int currentNumber = 0;
+            boolean hasNextInt = scanner.hasNextInt();
+
+            while (!hasNextInt) {
+                System.out.println("Invalid Number!");
+                scanner.nextLine();
+                hasNextInt = scanner.hasNextInt();
+            }
+            sum += scanner.nextInt();
+            scanner.nextLine();
+
+            numberCounter++;
+        }
+        System.out.println("Your sum is: " + sum);
+
+        scanner.close();
     }
 }
